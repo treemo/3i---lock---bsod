b602e9baed939e7c596482b0b5d2853e2912a564
Treemo <treemo@hotmail.fr>
2015-09-04 15:10:03 +0200

    import

---
+++ lock.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ 
+# Dependencies:
+# imagemagick
+ 
+IMAGE=/tmp/i3lock.png
+CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+#SCREEN_RESOLUTION=$(xrandr | grep \* | cut -d' ' -f4 | sort -n -r | head -n1)
+SCREEN_RESOLUTION=$(xrandr | grep +0+0 | cut -d ' ' -f3 | cut -d '+' -f1)
+
+convert $CURRENT_DIR/background.png -resize $SCREEN_RESOLUTION $IMAGE
+i3lock -f -i $IMAGE --color 0001AB
+rm $IMAGE
