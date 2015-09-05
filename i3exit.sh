5669a56beffa7c56c1a1e85ccecdb1ae1aa57913
Treemo <treemo@hotmail.fr>
2015-09-05 11:46:00 +0200

    add i3exit script

---
+++ i3exit.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+lock() {
+	CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+	$CURRENT_DIR/lock.sh
+}
+
+case "$1" in
+	lock)
+		lock
+		;;
+	logout)
+		i3-msg exit
+		;;
+	suspend)
+		gksu pm-suspend
+		;;
+	hibernate)
+		lock && dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Hibernate
+		;;
+	reboot)
+		dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Restart
+		;;
+	shutdown)
+		dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop
+		;;
+	*)
+	echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
+	exit 2
+esac
+
+exit 0
