f694c60e8ed3ad48799e050c3595a7cafc5d2da9
Treemo <treemo@hotmail.fr>
2017-01-23 16:43:55 +0100

    fix screen size detection

--- lock.sh
+++ lock.sh
@@ -5,9 +5,7 @@
  
 IMAGE=/tmp/i3lock.png
 CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-#SCREEN_RESOLUTION=$(xrandr | grep \* | cut -d' ' -f4 | sort -n -r | head -n1)
-#SCREEN_RESOLUTION=$(xrandr | grep +0+0 | cut -d ' ' -f3 | cut -d '+' -f1)
-SCREEN_RESOLUTION=$(xrandr | grep +0+0 | cut -d ' ' -f4 | cut -d '+' -f1)
+SCREEN_RESOLUTION=$(xrandr | xrandr | grep -E -o '\w*\+0\+0' | cut -d '+' -f1)
 
 convert $CURRENT_DIR/background.png -resize $SCREEN_RESOLUTION $IMAGE
 i3lock -f -i $IMAGE --color 0001AB
